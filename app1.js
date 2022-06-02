b5c0da223948c65f92b6b843b2a7811bf5b0bd85
Siddartha <sidhureddy.legala@gmail.com>
2022-06-02 18:14:30 +0530

    first commit

---
+++ app1.js
@@ -0,0 +1,20 @@
+
+// app.get("/demo", (req, res) => {
+//   res
+//     .status(200)
+//     .render("demo", {
+//       title: "Hey Harry",
+//       message: "Hello there and thanks for telling me how to use pubG!",
+//     }); // Our pug demo endpoint
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("this is home page of my first express app");
+// });
+// app.get("/about", (req, res) => {
+//   res.status(200).send("this is about page of my first express app");
+// });
+// app.post("/about", (req, res) => {
+//   res.send("this is a post request about page of my first express app");
+// });
+// const port = 80;
